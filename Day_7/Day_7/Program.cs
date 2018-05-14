d438e4b484219067ad6ba2d907194b7a121981fc
astikute <agnese.stikute@gmail.com>
2018-05-14 21:06:46 +0300

    7.diena
    Cikli

--- Day_7/Day_7/Program.cs
+++ Day_7/Day_7/Program.cs
@@ -10,6 +10,25 @@ namespace Day_7
     {
         static void Main(string[] args)
         {
+            //Uzdevums - izvadit ievadita skaitla faktorialu!
+
+            Console.Write("Ievadiet skaitli: ");
+            int sk = Convert.ToInt32(Console.ReadLine());
+            int rez = 0;
+
+            if (sk == 0) // Skaitla 0! = 1
+            {
+                Console.Write("0! = 1");
+            }
+            else
+            { 
+                for (int i = 1; i <= sk; i++) // Parejiem skaitliem, sakot no 1
+                {
+                    rez = rez + i;
+                }
+                Console.Write("{0}! = {1}", sk, rez);
+            }
+            Console.Read();
         }
     }
 }
